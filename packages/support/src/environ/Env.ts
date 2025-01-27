6078cfa6195c8d4c59ef2288f33a44cd739c8880
Ily1606 <40027165+Ily1606@users.noreply.github.com>
2025-01-27 02:25:52 +0000

    refactor: removed assertion

--- packages/support/src/environ/Env.ts
+++ packages/support/src/environ/Env.ts
@@ -3,6 +3,6 @@ import { EnvVariables } from "../contracts/Env";
 export default class Env {
 
   public static get<T extends keyof EnvVariables, D>(key: T, defaultVal: D) {
-    return (process.env as EnvVariables)[key] || defaultVal;
+    return (process.env)[key] || defaultVal;
   }
 }
