f64d74f3b125c3f08481fca38d1cc3c6556e2a94
Ily1606 <no1.ily1606@gmail.com>
2025-08-31 12:19:32 +0700

    feat: enhance Middleware and Request classes to include response handling in middleware processing

--- packages/http/src/middleware/Middleware.ts
+++ packages/http/src/middleware/Middleware.ts
@@ -1,8 +1,20 @@
 import { Middleware as MiddlewareContract } from "../contracts";
 import Request from "../supports/Request";
-export default class Middleware implements MiddlewareContract {
-  async handle(request: Request, next: (request: Request) => Promise<unknown> | unknown) {
+
+export default class Middleware<T = any> implements MiddlewareContract {
+
+  protected previousResult: T | undefined;
+
+  async handle(request: Request, res: Response, next: (request: Request) => Promise<unknown> | unknown) {
     // Middleware logic here
     return await next(request);
+  }
+
+  setPreviousResult(result: T) {
+    this.previousResult = result;
+  }
+
+  getPreviousResult(): T | undefined {
+    return this.previousResult;
   }
 }
