e83ddee3cbf5330bce77ca2cb9b7dc2a2ac4964d
Ily1606 <no1.ily1606@gmail.com>
2025-09-15 20:51:25 +0700

    refactor: update type imports and definitions in Controller and Middleware contracts

--- packages/http/src/contracts/index.ts
+++ packages/http/src/contracts/index.ts
@@ -2,5 +2,5 @@ export type { default as Collection } from "./Collection";
 export type * from "./Controller";
 export type * from "./Request";
 export type { default as Response }from "./Response";
-export type { default as Middleware, MiddlewareHandler }from "./Middleware";
+export type { default as Middleware, MiddlewareHandler, NextHandler }from "./Middleware";
 export type * from "./Route";
