f27e87619c1a79458c52c4216bfffe17b1dcff60
Ily1606 <no1.ily1606@gmail.com>
2025-09-15 23:25:12 +0700

    feat: define ServiceProviderType enum and extend service provider classes

--- packages/support/src/ServiceProvider.ts
+++ packages/support/src/ServiceProvider.ts
@@ -1,6 +1,14 @@
 import { Hono } from "hono";
 
+export enum ServiceProviderType {
+  CORE_PROVIDER = "core_provider",
+  APP_PROVIDER = "app_provider",
+  ROUTE_PROVIDER = "route_provider",
+};
+
 export class ServiceProvider {
+
+  static type = ServiceProviderType.CORE_PROVIDER;
 
   public register(): void | Promise<void> {
     
@@ -12,11 +20,33 @@ export class ServiceProvider {
 }
 
 export class AppServiceProvider extends ServiceProvider {
+  
+  static type = ServiceProviderType.APP_PROVIDER;
   public bindings = [];
   public apps: Hono;
 
   constructor(apps: Hono) {
     super();
     this.apps = apps;
+  }
+}
+
+export abstract class RouteAppServiceProvider extends AppServiceProvider {
+
+  static type = ServiceProviderType.ROUTE_PROVIDER;
+  public bindings = [];
+  public apps: Hono;
+
+  constructor(apps: Hono) {
+    super(apps);
+    this.apps = apps;
+  }
+  
+  public abstract routes(): Array<Record<string, any>>;
+
+  public boot(): void {
+    const routes = this.routes();
+
+    globalThis.__laratype_routes = routes;
   }
 }
